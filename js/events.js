4604c6800f493dc91417737f148c010d5a164196
Steven Luck <steve.luck@gmail.com>
2020-03-27 19:35:30 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -6,12 +6,20 @@ function getIt() {
 function frameIt() {
   $('img').addClass("tasty");
 }
+
+function pressIt() {
+  alert('hey! You pressed g!')
+}
+
 $(document).ready(function(){
 
 // call functions here
    {
     $('p').on("click", getIt());
     $('img').on("load", frameIt());
+    $('input').on("keydown", function(key) {
+      
+    })
   }
 });
 
